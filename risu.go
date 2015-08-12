3de24b5506c81f32abe97091bec5ce602b67ac9f
koudaiii <cs006061@gmail.com>
2015-08-12 12:12:49 +0900

    request for builds

--- risu.go
+++ risu.go
@@ -1,12 +1,34 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/codegangsta/negroni"
 	"github.com/julienschmidt/httprouter"
 )
+
+// Build is "Create a new build.""
+type Build struct {
+	SourceRepo     string `json:"source_repo"`
+	SourceRevision string `json:"source_revision"`
+	Name           string `json:"name"`
+	Dockerfile     string `json:"dockerfile"`
+}
+
+func create(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	defer r.Body.Close()
+	build := &Build{Dockerfile: "Dockerfile"} // default setup Dockerfile
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&build)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Fprintln(w, build)
+}
 
 func index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	name := r.FormValue("name")
@@ -21,7 +43,8 @@ func show(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 func main() {
 	router := httprouter.New()
 	router.GET("/", index)
-	router.GET("/build/:image", show)
+	router.GET("/builds/:image", show)
+	router.POST("/builds", create)
 
 	n := negroni.Classic()
 	n.UseHandler(router)
