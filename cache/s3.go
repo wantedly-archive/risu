f93ed71cb27225b3d6561853b7aa29c6da58ecdf
Daisuke Fujita <dtanshi45@gmail.com>
2015-08-13 22:02:13 +0900

    Implement getting cache from S3

--- cache/s3.go
+++ cache/s3.go
@@ -1,6 +1,7 @@
 package cache
 
 import (
+	"io/ioutil"
 	"os"
 	"path/filepath"
 
@@ -34,8 +35,50 @@ func NewS3Cache() Cache {
 }
 
 func (c *S3Cache) Get(key string) (string, error) {
-	// TODO
-	return "", nil
+	cachePath := cacheObjectPath(c.cacheBucket, key)
+	inflateDir := inflateDirPath(key)
+
+	_, err := c.s3Client.HeadObject(
+		&s3.HeadObjectInput{
+			Bucket: aws.String(c.cacheBucket),
+			Key:    aws.String(key),
+		})
+
+	if err != nil {
+		if awsErr, ok := err.(awserr.RequestFailure); ok && awsErr.StatusCode() == 404 {
+			return "", nil
+		}
+
+		return "", err
+	}
+
+	resp, err := c.s3Client.GetObject(
+		&s3.GetObjectInput{
+			Bucket: aws.String(c.cacheBucket),
+			Key:    aws.String(key),
+		})
+
+	if err != nil {
+		return "", err
+	}
+
+	defer resp.Body.Close()
+
+	data, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		return err
+	}
+
+	if err = ioutil.WriteFile(temporaryCache, data, 0644); err != nil {
+		return err
+	}
+
+	if err = InflateTarGz(temporaryCache, inflateDir); err != nil {
+		return err
+	}
+
+	return inflateDir, nil
 }
 
 func (c *S3Cache) Put(key, directory string) error {
@@ -65,6 +108,14 @@ func (c *S3Cache) Put(key, directory string) error {
 	}
 
 	return nil
+}
+
+func cacheObjectPath(bucket, key string) string {
+	return "s3://" + bucket + "/" + key
+}
+
+func inflateDirPath(key string) string {
+	return TemporaryCacheDir + string(filepath.Separator) + key
 }
 
 func temporaryCachePath(key string) string {
