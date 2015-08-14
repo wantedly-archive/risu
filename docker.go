4b9adb963dce32a6257c0172df81b655e6e7450f
Daisuke Fujita <dtanshi45@gmail.com>
2015-08-14 18:13:19 +0900

    Get cache

--- docker.go
+++ docker.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/fsouza/go-dockerclient"
 
+	c "github.com/wantedly/risu/cache"
 	"github.com/wantedly/risu/schema"
 )
 
@@ -14,7 +15,19 @@ const (
 	DefaultDockerEndpoint = "unix:///var/run/docker.sock"
 )
 
-func DockerBuild(build *schema.Build, cacheDir string) {
+func DockerBuild(build *schema.Build) {
+	cache := c.NewCache(os.Getenv("CACHE_BACKEND"))
+	inflatedCachePath, err := cache.Get(build.ID.String())
+
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	if inflatedCachePath != "" {
+		// put cache to repository
+	}
+
 	var dockerEndpoint string
 
 	if os.Getenv("DOCKER_HOST") != "" {
