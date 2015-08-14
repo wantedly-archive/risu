00c6bb6267c7573311ce3dbbf3f7f1b4e0a52e74
Naoyoshi Aikawa <n.aikawa91@gmail.com>
2015-08-14 13:56:17 +0900

    Add create test base

--- risu_test.go
+++ risu_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -24,4 +25,49 @@ func TestRootAccess(t *testing.T) {
 	if body != expectedBody {
 		t.Errorf("Got empty body for GET request to /\n Got: %s, Expected: %s", body, expectedBody)
 	}
+}
+
+func TestBuildFlow(t *testing.T) {
+	response := httptest.NewRecorder()
+
+	n := setUpServer()
+
+	requestParams := `{
+		"source_repo": "wantedly/risu",
+		"source_revision": "ada9ce1829fab49e605e5a563dbf91274f64e923",
+		"name": "quay.io/wantedly/risu:latest",
+		"dockerfile": "Dockerfile.dev"
+	}`
+
+	// Create
+	req, err := http.NewRequest("POST", "http://localhost:8080/builds", bytes.NewBuffer([]byte(requestParams)))
+	if err != nil {
+		t.Error(err)
+	}
+	n.ServeHTTP(response, req)
+	if response.Code != http.StatusOK {
+		t.Errorf("Got error for GET ruquest to /")
+	}
+	body := string(response.Body.Bytes())
+	if body != "hoge" {
+		t.Errorf("Got: %v", body)
+	}
+
+	// dec := json.NewDecoder(response.Body)
+	// var build schema.Build
+	// dec.Decode(&build)
+	// expectedBuild := schema.Build{
+	// 	ID:             uuid.NewUUID(),
+	// 	SourceRepo:     "opts.SourceRepo",
+	// 	SourceRevision: "opts.SourceRevision",
+	// 	Name:           "opts.Name",
+	// 	Dockerfile:     "Dockerfile",
+	// 	Status:         "building",
+	// 	CreatedAt:      time.Now(),
+	// 	UpdatedAt:      time.Now(),
+	// }
+	//
+	// if !build.Equals(expectedBuild) {
+	// 	t.Errorf("Got empty body for GET request to /\n Got: %v\nExpected: %v", build, expectedBuild)
+	// }
 }
